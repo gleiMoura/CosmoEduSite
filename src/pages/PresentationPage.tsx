cdd0a701b99282c6b763489276b17d2474ae9c64
gleiMoura <gle.bar.mou@gmail.com>
2024-07-26 15:49:26 -0300

    ✨ create a Presentation Page

---
+++ src/pages/PresentationPage.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react"
+import styled from "styled-components";
+import WhoAmI from "../assets/quem_sou_eu.png";
+import Header from "../components/Header";
+
+export const Presentation: FC = () => {
+    return (
+        <Main>
+            <Header />
+            <img src={WhoAmI} className="who_am_i" />
+        </Main>
+    )
+};
+
+const Main = styled.main`
+    width: 100%;
+    height: calc(100vh);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .who_am_i{
+        width: 50%
+    }
+`
+
