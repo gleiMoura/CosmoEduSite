b23bdfc7158286b704700d24d8742438446a4fc5
gleiMoura <gle.bar.mou@gmail.com>
2024-07-26 18:27:17 -0300

    ✨ feat: add Cards inside Portfolio page.

--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,11 +1,31 @@
 import { FC } from "react";
 import styled from "styled-components";
 import Title from "../components/Title";
+import Data from "../assets/data.json";
+import Card from "../components/Card";
+import Video from "../components/Video";
 
 export const Portfolio: FC = () => {
+    const cardsList = [...Data].reverse();
     return (
         <Main>
             <Title title="Portfólio" />
+
+            <div className="cards">
+                {cardsList.map((info, index) => {
+                    return (
+                        <Card
+                            key={index}
+                            title={info.title || "undefined"}
+                            subtitle={info.subtitle}
+                            text={info.text}
+                            alt={info.title}
+                            component={info.type === "frontend" ? <Video path={`/videos/video${info.title}.mp4`} /> : <img src={`/images/${info.title}.png`} />}
+                            color="#F2C744"
+                        />
+                    )
+                })}
+            </div>
         </Main>
     )
 };
@@ -14,4 +34,14 @@ const Main = styled.main`
     width: 100%;
     height: auto;
     min-height: 100vh;
-`
+
+    .cards {
+        width: 100%;
+        height: auto;
+        padding: 20px;
+        box-sizing: border-box;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+`;
