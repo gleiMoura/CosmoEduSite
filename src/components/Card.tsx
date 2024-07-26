b0887e44eb3f1524ebe8010939a663fdf562d2b7
gleiMoura <gle.bar.mou@gmail.com>
2024-07-26 18:26:05 -0300

    ♻️ refact: create prop text and change usebility from subtitle

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,19 +3,22 @@ import styled from "styled-components";
 
 interface CardProps {
     title: string,
+    subtitle?: string,
+    text?: string,
     image?: string,
     alt?: string,
-    subtitle?: string,
-    component?: ReactNode
+    component?: ReactNode,
+    color?: string
 };
 
-const Card: FC<CardProps> = ({ title, image, alt, subtitle, component }) => {
+const Card: FC<CardProps> = ({ title, subtitle, text, image, alt, component, color }) => {
     return (
-        <CardMain>
+        <CardMain style={{ backgroundColor: color }}>
             <h2>{title}</h2>
+            {subtitle && <h3>Tecnologias: {subtitle}</h3>}
             {image && <img src={image} alt={alt} />}
             {component && component}
-            {subtitle && <p>{subtitle}</p>}
+            {text && <p>{text}</p>}
         </CardMain>
     )
 };
@@ -24,15 +27,13 @@ export default Card;
 
 const CardMain = styled.section`
     width: 350px;
-    height: 350px;
+    height: 400px;
     margin: 10px 0;
     color: black;
     font-family: "Roboto";
     font-weight: 500;
-    font-size: 1.5rem;
-    background-color: white;
     border-radius: 10px;
-    padding: 15px;
+    padding: 30px;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
@@ -47,5 +48,11 @@ const CardMain = styled.section`
         font-size: 1.5rem;
         text-align: center;
         margin-bottom: 20px;
+    }
+    
+    p{
+        font-weight: 400;
+        font-size: 1rem;
+        text-align: start;
     }
 `
