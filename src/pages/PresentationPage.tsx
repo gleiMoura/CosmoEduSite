ef4277841c40154480ec97a2fb65105ec34185c4
gleiMoura <gle.bar.mou@gmail.com>
2024-08-21 15:25:37 -0300

    💄 style: change mobile presentation photo

--- src/pages/PresentationPage.tsx
+++ src/pages/PresentationPage.tsx
@@ -1,13 +1,29 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import styled from "styled-components";
 import IWhite from "/images/eu_claro.png";
 import IDark from "/images/eu_escuro.png";
+import mobile from "/images/mobile.png";
 import Header from "../components/Header";
 import { useContext } from "react";
 import { ModeContext } from "../contexts/ModeContext";
 
 export const Presentation: FC = () => {
     const modeContext = useContext(ModeContext);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function resize() {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', resize);
+
+        resize();
+
+        return () => {
+            window.removeEventListener('resize', resize);
+        }
+    }, []);
 
     if (!ModeContext) {
         throw new Error('ModeSwitcher deve ser usado dentro de um ModeProvider');
@@ -21,22 +37,32 @@ export const Presentation: FC = () => {
     return (
         <Main mode={mode}>
             <Header changeMode={changeMode} mode={mode} />
-            <img src={mode ? IDark : IWhite} className="who_am_i" />
+            <div className="image">
+                {windowWidth >= 700 ?
+                    <img src={mode ? IDark : IWhite} width={'53%'} /> :
+                    <img src={mobile} width="100%" className="short" />}
+            </div>
+
         </Main>
     )
 };
 
 const Main = styled.main<{ mode: boolean }>`
     width: 100%;
-    height: 100vh;
+    height: auto;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: center;
     background-color: ${(props) => props.mode ? "#21272F" : "white"};
 
-    .who_am_i{
-        width: 50%;
-        margin-top: 20px;
-    }
+    .image {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 30px;
+        box-sizing: border-box;
+   }
 `
 
