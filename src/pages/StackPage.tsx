858e23a098fc475506a0dbbb928556d5cf034001
gleiMoura <gle.bar.mou@gmail.com>
2024-08-21 15:33:02 -0300

    💄 style: organizing Stack Page to mobile

--- src/pages/StackPage.tsx
+++ src/pages/StackPage.tsx
@@ -42,7 +42,9 @@ export const Stack: FC = () => {
 
 const Main = styled.main<{ mode: boolean }>`
     width: 100%;
-    height: 100vh;
+    min-height: 100vh;
+    max-height: auto;
+    padding: 20px 0;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -55,6 +57,7 @@ const Main = styled.main<{ mode: boolean }>`
 
 const Cards = styled.div`
     width: 100%;
+    flex-wrap: wrap;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
